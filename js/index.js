01cf9809cae9eb1ef47032c534dfaebedb9fe6ab
Sierra <sierra.ar.sager@gmail.com>
2025-01-19 14:33:15 -0500

    Completed experiment and commented out code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let healthPoints = 15;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+let coffees = true;
+
+if (coffees === true) {
+    console.log("You've been caffeinated. You may continue with your day.")
+}
+
+let coffees = 1;
+
+if (coffees <= 2 ) {
+    console.log("You're not awake yet. Have more coffee.");
+} else {
+    console.log("Congratulations, you're awake!");
+}
+
+
+let coffees = 6;
+
+if (coffees <= 2) {
+    console.log("You're not awake yet. Have more coffee.");
+} else if (coffees > 2 && coffees <= 5) {
+    console.log("Congratulations, you're awake!");
+} else {
+    console.log("You're going to vibrate out of this dimension.");
+}
+
+*/
